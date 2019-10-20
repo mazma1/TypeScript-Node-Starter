cb3cb685c2c5408cf4b40e421b2d43162e68f4e6
Mary Mazi <mazi.mary.o@gmail.com>
2019-10-20 04:03:41 +0100

    add script to deploy via helm

---
+++ release-app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+TAG=$1
+APP="node-starter-app"
+
+export HELM_HOST=127.0.0.1:44134
+export KUBECONFIG=$HOME/.kube/kubeconfig
+
+echo "Starting Tiller..."
+
+helm tiller start-ci
+result=$(helm ls | grep $APP) 
+
+if [ $? -ne "0" ]; then 
+   helm install --timeout 180 --name $APP --set image.tag=$TAG charts/ts-node-starter-chart
+else 
+   helm upgrade --timeout 180 $APP --set image.tag=$TAG charts/ts-node-starter-chart
+fi
+
+echo "Stoping Tiller..."
+helm tiller stop 

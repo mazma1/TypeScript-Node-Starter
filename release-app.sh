37b78f3332a1cdd75008e077586945dc7e086307
Mary Mazi <mazi.mary.o@gmail.com>
2019-10-21 11:57:37 +0100

    debug tiller connection error

--- release-app.sh
+++ release-app.sh
@@ -6,8 +6,15 @@ export KUBECONFIG=$HOME/.kube/kubeconfig
 
 echo "Starting Tiller..."
 
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+
 helm tiller start-ci
+
 export HELM_HOST=127.0.0.1:44134
+
+helm version
 
 result=$(helm ls | grep $APP) 
 

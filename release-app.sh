406e6d339317836e29962d256943373f64d18325
Mary Mazi <mazi.mary.o@gmail.com>
2019-10-29 07:32:16 +0100

    debug missing TAG value in realease-app script

--- release-app.sh
+++ release-app.sh
@@ -6,9 +6,11 @@ export KUBECONFIG=$HOME/.kube/kubeconfig
 
 echo "Starting Tiller..."
 
-kubectl create serviceaccount --namespace kube-system tiller
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+echo "${TAG}, <==="
+
+# kubectl create serviceaccount --namespace kube-system tiller
+# kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+# kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 
 helm tiller start-ci
 

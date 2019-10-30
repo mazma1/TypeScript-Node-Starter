71765e801a7d7c6482fce651244da0f7e49f5604
Mary Mazi <mazi.mary.o@gmail.com>
2019-10-30 08:29:40 +0100

    yet another path uodate

--- release-app.sh
+++ release-app.sh
@@ -5,6 +5,8 @@ RELEASE_NAME="node-starter-app-release"
 export KUBECONFIG=$HOME/.kube/kubeconfig
 
 echo "Starting Tiller..."
+pwd
+ls ./ts-node-starter-chart
 
 helm tiller start-ci
 export HELM_HOST=127.0.0.1:44134
@@ -12,10 +14,10 @@ result=$(helm ls | grep $RELEASE_NAME)
 
 if [ $? -ne "0" ]; then 
    echo 'running helm install'
-   helm install --timeout 180 --name $RELEASE_NAME --values values-staging.yaml --set image.tag=$TAG ts-node-starter-chart
+   helm install --timeout 180 --name $RELEASE_NAME --values ./ts-node-starter-chart/values-staging.yaml --set image.tag=$TAG ts-node-starter-chart
 else
    echo 'running helm upgrade'
-   helm upgrade --timeout 180 $RELEASE_NAME --values values-staging.yaml --set image.tag=$TAG ts-node-starter-chart
+   helm upgrade --timeout 180 $RELEASE_NAME --values ./ts-node-starter-chart/values-staging.yaml --set image.tag=$TAG ts-node-starter-chart
 fi
 
 echo "Stoping Tiller..."
